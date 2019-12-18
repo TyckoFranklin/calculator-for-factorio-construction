2c89e869b9df4f84e6a51f40c8a6cff5381be401
AlexanderFranklin <roaringdragon02@gmail.com>
2019-12-18 00:22:34 -0800

    Added a few divs and some style. Will need to automate these divs a bit and fill them with recipe information.

--- src/views/Content/Content.js
+++ src/views/Content/Content.js
@@ -4,6 +4,16 @@ import './Content.css';
 function Content(props) {
     return (
         <div className="content">
+            <div className="recipe" id="viewedItem">Engine unit</div>
+            <div className="requires">
+                <div className="recipe">Gear</div>
+                <div className="recipe">Pipe</div>
+            </div>
+            <div className="usedFor">
+                <div className="recipe">Car</div>
+                <div className="recipe">Electric engine unit</div>
+                <div className="recipe">Tank</div>
+            </div>
         </div>
     );
 }
