7f3b2757d198491fafc8bae2355c87f087b35344
Tycko Franklin <TheTyckoMan@gmail.com>
2019-12-17 23:06:01 -0800

    Impelemented basic gui design elements.
    Co-authored-by: RoaringDragon2 <roaringdragon02@gmail.com>

--- src/views/App.js
+++ src/views/App.js
@@ -1,33 +1,17 @@
 import React from 'react';
 import logo from '../logo.svg';
 import './App.css';
+import Header from './Header/_Header'
+import Panel from './Panel/_Panel'
+import Content from './Content/_Content'
 
 function App(props) {
     props.demo("hey there!!!!");
   return (
     <div className="App">
-    {
-        /*
-            Menu Bar Component
-            Side Bar Component
-            Main area content component
-        */
-
-    }
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header />
+        <Panel />
+        <Content />
     </div>
   );
 }
