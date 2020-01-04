f69af687a6b47c6c8020d6be6406255573fe2c35
Tycko Franklin <TheTyckoMan@gmail.com>
2020-01-03 22:21:02 -0800

    add graphql queries

---
+++ src/graphql/queries.js
@@ -0,0 +1,233 @@
+// eslint-disable
+// this is an auto generated file. This will be overwritten
+
+export const getEntity = `query GetEntity($id: ID!) {
+  getEntity(id: $id) {
+    id
+    name
+    category {
+      id
+      gameId
+      entities {
+          items {
+            id
+          }
+        nextToken
+      }
+    }
+  }
+}
+`;
+export const listEntitys = `query ListEntitys(
+  $filter: ModelentityFilterInput
+  $limit: Int
+  $nextToken: String
+) {
+  listEntitys(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    items {
+      id
+      gameId
+      name
+      wiki_link
+      gameItemType {
+        id
+        gameId
+        owner
+      }
+      category {
+        id
+        gameId
+        owner
+      }
+      recipes {
+        nextToken
+      }
+      owner
+    }
+    nextToken
+  }
+}
+`;
+export const getRecipe = `query GetRecipe($id: ID!) {
+  getRecipe(id: $id) {
+    id
+    time
+    yield
+    ingredients {
+      items {
+        id
+        gameId
+        amount
+        owner
+      }
+      nextToken
+    }
+    entity {
+      id
+      gameId
+      name
+      wiki_link
+      gameItemType {
+        id
+        gameId
+        owner
+      }
+      category {
+        id
+        gameId
+        owner
+      }
+      recipes {
+        nextToken
+      }
+      owner
+    }
+    owner
+  }
+}
+`;
+export const listRecipes = `query ListRecipes(
+  $filter: ModelrecipeFilterInput
+  $limit: Int
+  $nextToken: String
+) {
+  listRecipes(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    items {
+      id
+      time
+      yield
+      ingredients {
+        nextToken
+      }
+      entity {
+        id
+        gameId
+        name
+        wiki_link
+        owner
+      }
+      owner
+    }
+    nextToken
+  }
+}
+`;
+export const getIngredient = `query GetIngredient($id: ID!) {
+  getIngredient(id: $id) {
+    id
+    gameId
+    amount
+    entity {
+      id
+      time
+      yield
+      ingredients {
+        nextToken
+      }
+      entity {
+        id
+        gameId
+        name
+        wiki_link
+        owner
+      }
+      owner
+    }
+    owner
+  }
+}
+`;
+export const listIngredients = `query ListIngredients(
+  $filter: ModelingredientFilterInput
+  $limit: Int
+  $nextToken: String
+) {
+  listIngredients(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    items {
+      id
+      gameId
+      amount
+      entity {
+        id
+        time
+        yield
+        owner
+      }
+      owner
+    }
+    nextToken
+  }
+}
+`;
+export const getCategory = `query GetCategory($id: ID!) {
+  getCategory(id: $id) {
+    id
+    gameId
+    entities {
+      items {
+        id
+        gameId
+        name
+        wiki_link
+        owner
+      }
+      nextToken
+    }
+    owner
+  }
+}
+`;
+export const listCategorys = `query ListCategorys(
+  $filter: ModelcategoryFilterInput
+  $limit: Int
+  $nextToken: String
+) {
+  listCategorys(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    items {
+      id
+      gameId
+      entities {
+        nextToken
+      }
+      owner
+    }
+    nextToken
+  }
+}
+`;
+export const getGameItemType = `query GetGameItemType($id: ID!) {
+  getGameItemType(id: $id) {
+    id
+    gameId
+    entities {
+      items {
+        id
+        gameId
+        name
+        wiki_link
+        owner
+      }
+      nextToken
+    }
+    owner
+  }
+}
+`;
+export const listGameItemTypes = `query ListGameItemTypes(
+  $filter: ModelgameItemTypeFilterInput
+  $limit: Int
+  $nextToken: String
+) {
+  listGameItemTypes(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    items {
+      id
+      gameId
+      entities {
+        nextToken
+      }
+      owner
+    }
+    nextToken
+  }
+}
+`;
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,405 @@
+// eslint-disable
+// this is an auto generated file. This will be overwritten
+
+export const onCreateEntity = `subscription OnCreateEntity {
+  onCreateEntity {
+    id
+    gameId
+    name
+    wiki_link
+    gameItemType {
+      id
+      gameId
+      entities {
+        nextToken
+      }
+      owner
+    }
+    category {
+      id
+      gameId
+      entities {
+        nextToken
+      }
+      owner
+    }
+    recipes {
+      items {
+        id
+        time
+        yield
+        owner
+      }
+      nextToken
+    }
+    owner
+  }
+}
+`;
+export const onUpdateEntity = `subscription OnUpdateEntity {
+  onUpdateEntity {
+    id
+    gameId
+    name
+    wiki_link
+    gameItemType {
+      id
+      gameId
+      entities {
+        nextToken
+      }
+      owner
+    }
+    category {
+      id
+      gameId
+      entities {
+        nextToken
+      }
+      owner
+    }
+    recipes {
+      items {
+        id
+        time
+        yield
+        owner
+      }
+      nextToken
+    }
+    owner
+  }
+}
+`;
+export const onDeleteEntity = `subscription OnDeleteEntity {
+  onDeleteEntity {
+    id
+    gameId
+    name
+    wiki_link
+    gameItemType {
+      id
+      gameId
+      entities {
+        nextToken
+      }
+      owner
+    }
+    category {
+      id
+      gameId
+      entities {
+        nextToken
+      }
+      owner
+    }
+    recipes {
+      items {
+        id
+        time
+        yield
+        owner
+      }
+      nextToken
+    }
+    owner
+  }
+}
+`;
+export const onCreateRecipe = `subscription OnCreateRecipe {
+  onCreateRecipe {
+    id
+    time
+    yield
+    ingredients {
+      items {
+        id
+        gameId
+        amount
+        owner
+      }
+      nextToken
+    }
+    entity {
+      id
+      gameId
+      name
+      wiki_link
+      gameItemType {
+        id
+        gameId
+        owner
+      }
+      category {
+        id
+        gameId
+        owner
+      }
+      recipes {
+        nextToken
+      }
+      owner
+    }
+    owner
+  }
+}
+`;
+export const onUpdateRecipe = `subscription OnUpdateRecipe {
+  onUpdateRecipe {
+    id
+    time
+    yield
+    ingredients {
+      items {
+        id
+        gameId
+        amount
+        owner
+      }
+      nextToken
+    }
+    entity {
+      id
+      gameId
+      name
+      wiki_link
+      gameItemType {
+        id
+        gameId
+        owner
+      }
+      category {
+        id
+        gameId
+        owner
+      }
+      recipes {
+        nextToken
+      }
+      owner
+    }
+    owner
+  }
+}
+`;
+export const onDeleteRecipe = `subscription OnDeleteRecipe {
+  onDeleteRecipe {
+    id
+    time
+    yield
+    ingredients {
+      items {
+        id
+        gameId
+        amount
+        owner
+      }
+      nextToken
+    }
+    entity {
+      id
+      gameId
+      name
+      wiki_link
+      gameItemType {
+        id
+        gameId
+        owner
+      }
+      category {
+        id
+        gameId
+        owner
+      }
+      recipes {
+        nextToken
+      }
+      owner
+    }
+    owner
+  }
+}
+`;
+export const onCreateIngredient = `subscription OnCreateIngredient {
+  onCreateIngredient {
+    id
+    gameId
+    amount
+    entity {
+      id
+      time
+      yield
+      ingredients {
+        nextToken
+      }
+      entity {
+        id
+        gameId
+        name
+        wiki_link
+        owner
+      }
+      owner
+    }
+    owner
+  }
+}
+`;
+export const onUpdateIngredient = `subscription OnUpdateIngredient {
+  onUpdateIngredient {
+    id
+    gameId
+    amount
+    entity {
+      id
+      time
+      yield
+      ingredients {
+        nextToken
+      }
+      entity {
+        id
+        gameId
+        name
+        wiki_link
+        owner
+      }
+      owner
+    }
+    owner
+  }
+}
+`;
+export const onDeleteIngredient = `subscription OnDeleteIngredient {
+  onDeleteIngredient {
+    id
+    gameId
+    amount
+    entity {
+      id
+      time
+      yield
+      ingredients {
+        nextToken
+      }
+      entity {
+        id
+        gameId
+        name
+        wiki_link
+        owner
+      }
+      owner
+    }
+    owner
+  }
+}
+`;
+export const onCreateCategory = `subscription OnCreateCategory {
+  onCreateCategory {
+    id
+    gameId
+    entities {
+      items {
+        id
+        gameId
+        name
+        wiki_link
+        owner
+      }
+      nextToken
+    }
+    owner
+  }
+}
+`;
+export const onUpdateCategory = `subscription OnUpdateCategory {
+  onUpdateCategory {
+    id
+    gameId
+    entities {
+      items {
+        id
+        gameId
+        name
+        wiki_link
+        owner
+      }
+      nextToken
+    }
+    owner
+  }
+}
+`;
+export const onDeleteCategory = `subscription OnDeleteCategory {
+  onDeleteCategory {
+    id
+    gameId
+    entities {
+      items {
+        id
+        gameId
+        name
+        wiki_link
+        owner
+      }
+      nextToken
+    }
+    owner
+  }
+}
+`;
+export const onCreateGameItemType = `subscription OnCreateGameItemType {
+  onCreateGameItemType {
+    id
+    gameId
+    entities {
+      items {
+        id
+        gameId
+        name
+        wiki_link
+        owner
+      }
+      nextToken
+    }
+    owner
+  }
+}
+`;
+export const onUpdateGameItemType = `subscription OnUpdateGameItemType {
+  onUpdateGameItemType {
+    id
+    gameId
+    entities {
+      items {
+        id
+        gameId
+        name
+        wiki_link
+        owner
+      }
+      nextToken
+    }
+    owner
+  }
+}
+`;
+export const onDeleteGameItemType = `subscription OnDeleteGameItemType {
+  onDeleteGameItemType {
+    id
+    gameId
+    entities {
+      items {
+        id
+        gameId
+        name
+        wiki_link
+        owner
+      }
+      nextToken
+    }
+    owner
+  }
+}
+`;
