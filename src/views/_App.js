8b3077555ec5cc08810510a9831810809a589d65
Tycko Franklin <TheTyckoMan@gmail.com>
2020-01-09 17:44:49 -0800

    Fetch All Data On Start

--- src/views/_App.js
+++ src/views/_App.js
@@ -13,7 +13,11 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
     return {
         fetchRecipes: () => {
+            dispatch({type: actions.FETCH_GAME_TYPES})
+            dispatch({type: actions.FETCH_CATEGORIES})
+            dispatch({type: actions.FETCH_ENTITIES})
             dispatch({type: actions.FETCH_RECIPES})
+            dispatch({type: actions.FETCH_INGREDIENTS})
         }
         // createSlideshow: (payload)=>{
         //     dispatch({type: actions.CREATE_SLIDESHOW, payload});
